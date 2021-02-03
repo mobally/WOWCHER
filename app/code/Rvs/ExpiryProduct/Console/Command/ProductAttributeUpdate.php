7722580d2a91d9b52a53e212a810a0b4a938d49c
Rvsmedia <help@rvsmedia.co.uk>
2021-02-03 12:16:59 +0000

    push from development date/time

---
+++ app/code/Rvs/ExpiryProduct/Console/Command/ProductAttributeUpdate.php
@@ -0,0 +1,36 @@
+<?php
+namespace Rvs\ExpiryProduct\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+
+class ProductAttributeUpdate extends Command
+{
+protected $path;
+
+	public function __construct(
+	\Rvs\ExpiryProduct\Model\UpdateAttrProducts $path,
+	string $name = null
+	) {
+	parent::__construct($name);
+	    $this->path = $path;
+	 }
+   protected function configure()
+   {
+       $this->setName('wowcher:catalog:update-attribute-products');
+       $this->setDescription('Update product attribute');
+       
+       parent::configure();
+   }
+   protected function execute(InputInterface $input, OutputInterface $output)
+   {
+   
+   $this->path->execute();  
+   }
+}  
+
+
+
