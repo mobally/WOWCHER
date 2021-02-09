add7740f4f78aa14cadb3f94f7314fafe7fb4786
Rvsmedia <help@rvsmedia.co.uk>
2021-02-09 04:42:49 +0000

    live changes

--- app/code/Rvs/ExpiryProduct/Plugin/ProductUrl.php
+++ app/code/Rvs/ExpiryProduct/Plugin/ProductUrl.php
@@ -4,6 +4,8 @@ namespace Rvs\ExpiryProduct\Plugin;
 class ProductUrl 
 {
 
+
+
     /**
      * @var \Magento\Store\Model\StoreManagerInterface
      */
@@ -22,25 +24,34 @@ class ProductUrl
     ) {
 		$urlInterface = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\UrlInterface');
 		$url = $urlInterface->getCurrentUrl();
+		$url_components = parse_url( $url, $component = -1 );	
+		
 		$word = "gclid";
-		if(strpos($url, $word) !== false){
+		if(array_key_exists('query', $url_components)){
+			
+			$query = $url_components['query'];
 			$url = $urlInterface->getCurrentUrl();
 			$gclidValue ='';
-			$gclidsplit = explode("gclid=",$url);	
-			$gclidValue = isset($gclidsplit[1]) ? $gclidsplit[1]:'null';
+			$querysplit = explode("&",$query);	
+			//$gclidValue = isset($gclidsplit[1]) ? $gclidsplit[1]:'null';
 			
 			if(empty($params) || (!empty($params) && !array_key_exists('_query', $params))){
 				
 				if(!array_key_exists('_query', $params)){
 					$params['_query'] = [];
 				}
-				if(!array_key_exists('gclid', $params['_query'])){
-					 $params['_query']['gclid'] = $gclidValue;
+			foreach($querysplit as $splitquery){
+				$quermain =  explode("=",$splitquery);
+				$queryname = $quermain[0];
+				$queryvalue = $quermain[1];
+				
+				if(!array_key_exists($queryname, $params['_query'])){
+					 $params['_query'][$queryname] = $queryvalue;
 				}
 
 			}
+			}			
 		} 
-		
 		
         return [
             $product,
