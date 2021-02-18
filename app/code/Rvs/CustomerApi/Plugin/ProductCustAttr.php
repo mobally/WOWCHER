2623dd4f0258b34cea1893e298df525dea58560a
Rvsmedia <help@rvsmedia.co.uk>
2021-02-18 06:52:21 +0000

    update code for url issue in api

--- app/code/Rvs/CustomerApi/Plugin/ProductCustAttr.php
+++ app/code/Rvs/CustomerApi/Plugin/ProductCustAttr.php
@@ -15,6 +15,19 @@ class ProductCustAttr
     	$objectManager =\Magento\Framework\App\ObjectManager::getInstance();
 	$helperImport = $objectManager->get('\Magento\Catalog\Helper\Image');
         $product = $entity;
+         $cats = $product->getCategoryIds();
+         $firstCategoryId = "";
+                                if(count($cats) ){
+
+                                    if($cats[0]==2){
+                                        $firstCategoryId = $cats[1];
+                                    }
+                                    else{
+                                          $firstCategoryId = $cats[0];
+                                    }
+                                    }
+	$product_url = $product->setCategoryId($firstCategoryId)->getProductUrl();
+                            
         /** Get Current Extension Attributes from Product */
         $extensionAttributes = $product->getExtensionAttributes();
         $imageUrl = $helperImport->init($product, 'product_page_image')
@@ -22,6 +35,7 @@ class ProductCustAttr
                 ->resize(250,250)
                 ->getUrl();
         $extensionAttributes->setGoogleImage($imageUrl); // custom field value set
+        $extensionAttributes->setGoogleCategoryProductUrl($product_url);
         $product->setExtensionAttributes($extensionAttributes);
         return $product;
     }
@@ -36,6 +50,19 @@ class ProductCustAttr
         
         $products = [];
         foreach ($searchCriteria->getItems() as $entity) {
+        $product = $entity;
+        $cats = $product->getCategoryIds();
+         $firstCategoryId = "";
+                                if(count($cats) ){
+
+                                    if($cats[0]==2){
+                                        $firstCategoryId = $cats[1];
+                                    }
+                                    else{
+                                          $firstCategoryId = $cats[0];
+                                    }
+                                    }
+	$product_url = $product->setCategoryId($firstCategoryId)->getProductUrl();
             /** Get Current Extension Attributes from Product */
             $extensionAttributes = $entity->getExtensionAttributes();
         $imageUrl = $helperImport->init($entity, 'product_page_image')
@@ -43,7 +70,7 @@ class ProductCustAttr
                 ->resize(250,250)
                 ->getUrl();
         $extensionAttributes->setGoogleImage($imageUrl); // custom field value set
-        
+        $extensionAttributes->setGoogleCategoryProductUrl($product_url);
             $entity->setExtensionAttributes($extensionAttributes);
             $products[] = $entity;
         }
