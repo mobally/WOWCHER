a5653fe0869a2dd5c5be76aa09f88220128de718
Rvsmedia <help@rvsmedia.co.uk>
2021-02-24 10:20:03 +0000

    add new deal file

---
+++ app/code/Rvs/ExpiryProduct/Model/Grouplist.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Rvs\ExpiryProduct\Model;
+
+class Grouplist 
+{
+	protected $productCollectionFactory;
+	protected $_date;
+	protected $dateTime;
+	private $state;
+	protected $productAction;
+	public function __construct(
+    \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+    \Magento\Framework\Stdlib\DateTime\TimezoneInterface $date,
+    \Magento\Framework\Stdlib\DateTime\DateTime $dateTime,
+    \Magento\Catalog\Model\ProductFactory $productFactory,
+    \Magento\Framework\App\State $state,
+    \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+    \Magento\Catalog\Model\ResourceModel\Product\Action $productAction,
+    \Magento\Store\Model\StoreManagerInterface $storeManager
+  ) {
+    $this->productCollectionFactory = $productCollectionFactory;
+    $this->_date =  $date;    
+    $this->dateTime = $dateTime;
+    $this->productFactory = $productFactory;
+    $this->state = $state;
+    $this->productRepository = $productRepository;
+    $this->productAction = $productAction;
+    $this->_storeManager = $storeManager;
+}
+
+	public function getBottomtList()
+   {
+   
+       $groupProductCollection = $this->productCollectionFactory->create() 
+                                        ->addAttributeToSelect('*')
+                                        ->addAttributeToFilter('type_id', array('eq' => 'grouped'))
+                                        ->addAttributeToFilter('status',\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED)
+                                        ->setPage(4,6);
+                                        
+                     return $groupProductCollection;  
+       
+}
+
+
+	public function getLeftList()
+   	{
+   
+       $groupProductCollection = $this->productCollectionFactory->create() 
+                                        ->addAttributeToSelect('*')
+                                        ->addAttributeToFilter('type_id', array('eq' => 'grouped'))
+                                        ->addAttributeToFilter('status',\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED)
+                                        ->setPage(2,8);
+                                        
+                     return $groupProductCollection;  
+       }
+
+}
