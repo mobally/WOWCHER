df7cfa2ba42dc83c105896701ce3c8d37ce317ba
Rvsmedia <help@rvsmedia.co.uk>
2021-03-09 06:29:28 +0000

    push code for display product sku & business id in order grid

---
+++ app/code/HP/OrderGrid/Ui/Component/Listing/Column/Businessid.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace HP\Ordergrid\Ui\Component\Listing\Column;
+
+use \Magento\Sales\Api\OrderRepositoryInterface;
+use \Magento\Framework\View\Element\UiComponent\ContextInterface;
+use \Magento\Framework\View\Element\UiComponentFactory;
+use \Magento\Ui\Component\Listing\Columns\Column;
+use \Magento\Framework\Api\SearchCriteriaBuilder;
+
+
+class Businessid extends Column
+{
+   protected $_orderRepository;
+    protected $_searchCriteria;
+    private $productRepository;
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilder $criteria,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->_orderRepository = $orderRepository;
+        $this->_searchCriteria  = $criteria;
+        $this->productRepository = $productRepository;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+    
+    public function loadMyProduct($sku)
+{
+    return $this->productRepository->get($sku);
+}
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$items) {
+                $productArr = [];
+                $order  = $this->_orderRepository->get($items["entity_id"]);
+
+                foreach ($order->getAllVisibleItems() as $item) {
+                    $sku = $item->getSku();
+                    $item_data = $this->loadMyProduct($sku);
+                    $productArr[] = $item_data->getBusinessId();
+                }
+                $items['products_business_id'] = implode(' - ', $productArr);
+                unset($productArr);
+            }
+        }
+        return $dataSource;
+    }
+}
