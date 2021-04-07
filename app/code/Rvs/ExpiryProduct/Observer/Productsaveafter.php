99f10abdc092f692fb596c4da80c0e83b6ed562d
Rvsmedia <help@rvsmedia.co.uk>
2021-04-07 07:26:42 +0000

    update json format issue code

--- app/code/Rvs/ExpiryProduct/Observer/Productsaveafter.php
+++ app/code/Rvs/ExpiryProduct/Observer/Productsaveafter.php
@@ -6,9 +6,33 @@ use Magento\Framework\Event\ObserverInterface;
 
 class Productsaveafter implements ObserverInterface
 {    
+
+protected $_request;
+
+	public function __construct(
+	    \Magento\Framework\App\RequestInterface $request
+	) {
+	    $this->_request = $request;
+	}
+
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $_product = $observer->getEvent()->getProduct();
+        $data = $this->_request->getPost();
+	    $type_id = $observer->getProduct()->getOrigData('type_id');
+	    $pro_status = $data['product']['status'];
+	    
+	    $json_productdisplay_row = $data['product']['productdisplay_row'];
+	    $json_productdisplay_column = $data['product']['productdisplay_column'];
+	    $result_row = json_decode($json_productdisplay_row);
+	   $result_column = json_decode($json_productdisplay_column);
+	   if($result_row == '' && $type_id == 'grouped'){
+        throw new \Magento\Framework\Exception\LocalizedException(__('Fix error before enabling this product - JSON format is invalid in productDisplay.row attribute'));
+  	}
+	   
+	   if($result_column == '' && $type_id == 'grouped'){
+        throw new \Magento\Framework\Exception\LocalizedException(__('Fix error before enabling this product - JSON format is invalid in productdisplay_column'));
+  	}
         $status = $_product->getStatus();
 	if($status == 2){
 	$_product->setDealStatus(12);
